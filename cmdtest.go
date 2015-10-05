d28ba866c665261e1b6913a8fbcf466170b218a7
David Johansson <david.johanssonsvt.se>
2015-10-05 09:39:06 +0200

    Starting area

--- cmdtest.go
+++ cmdtest.go
@@ -5,8 +5,8 @@ import (
 	"github.com/codegangsta/cli"
 	"github.com/davidjohansson/ecmdsolrsearch"
 	"github.com/davidjohansson/ecmdinspect"
+	"github.com/davidjohansson/ecmdarea"
 	"bufio"
-	"fmt"
 	"strings"
 )
 
@@ -28,28 +28,37 @@ func main() {
 			Name:      "article",
 			Aliases:   []string{"a"},
 			Usage:     "inspects an article",
-			Flags:	 []cli.Flag {
-              cli.StringFlag{
-                Name: "fields, f",
-                Usage: "comma separated list of fields to display",
-              },
-    		cli.StringFlag{
-			  Name: "relation, r",
-			  Usage: "relation to display",
+			Flags:     []cli.Flag {
+				cli.StringFlag{
+					Name: "fields, f",
+					Usage: "comma separated list of fields to display",
+				},
+				cli.StringFlag{
+					Name: "relation, r",
+					Usage: "relation to display",
+				},
 			},
-            },
-
 			Action: func(c *cli.Context) {
-
 				ids := c.Args()
-			
 				if len(ids) == 0 {
 					reader := bufio.NewReader(os.Stdin)
-		    		text, _ := reader.ReadString('\n')
+					text, _ := reader.ReadString('\n')
 					ids = strings.Split(text, " ")
-
-					}
-					ecmdinspect.Inspect(c.String("fields"), ids)
+				}
+				ecmdinspect.Inspect(c.String("fields"), ids)
+			},
+		},
+		{
+			Name:      "area",
+			Usage:     "inspects contents of an area",
+			Action: func(c *cli.Context) {
+				ids := c.Args()
+				if len(ids) == 0 {
+					reader := bufio.NewReader(os.Stdin)
+					text, _ := reader.ReadString('\n')
+					ids = strings.Split(text, " ")
+				}
+				ecmdarea.ListArea(c.Args().Get(0), c.Args().Get(1))
 			},
 		},
 	}
